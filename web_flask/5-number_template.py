14bdaea154f56056885099b3475dccc74e61ed5b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-11-12 10:26:53 +0100

    Jinja template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -55,7 +55,7 @@ def int_only(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def num_int(n):
     """display a HTML page only if n is an integer"""
     return (render_template('5-number.html', number=n))
