e32f8dff4058a8250eae40a2a87283b779ecc174
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-11-13 14:49:37 +0100

    rendered an HTML page from a database where state.id == city.id

--- models/state.py
+++ models/state.py
@@ -5,7 +5,9 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 import models
+from os import getenv
 
+database = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """ State class """
@@ -14,18 +16,19 @@ class State(BaseModel, Base):
     cities = relationship("City",
                           cascade="all, delete-orphan", backref="state")
 
-    @property
-    def cities(self):
-        """return a list City instances with state_id
-        equals to the current State.id"""
-        all_obj = models.storage.all()
-        city_ins = []
-        same_id = []
-        for key in all_obj.keys():
-            key_list = key.split('.')
-            if key_list[0] == 'City':
-                city_ins.append(all_obj[key])
-        for state in city_ins:
-            if state.state_id == self.id:
-                same_id.append(state)
-        return(same_id)
+    if database != "db":
+        @property
+        def cities(self):
+            """return a list City instances with state_id
+            equals to the current State.id"""
+            all_obj = models.storage.all()
+            city_ins = []
+            same_id = []
+            for key in all_obj.keys():
+                key_list = key.split('.')
+                if key_list[0] == 'City':
+                    city_ins.append(all_obj[key])
+            for state in city_ins:
+                if state.state_id == self.id:
+                    same_id.append(state)
+            return(same_id)
